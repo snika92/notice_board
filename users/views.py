abb3e5ec752419fca7174a39a931ea578f2b8394
Veronika Surkova <surkova_nika92@mail.ru>
2025-05-01 19:26:00 +0300

    Add user's views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework.generics import (CreateAPIView, DestroyAPIView,
+                                     ListAPIView, RetrieveAPIView,
+                                     UpdateAPIView)
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .models import User
+from .permissions import IsAdmin, IsUser
+from .serializers import UserSerializer, UserDetailSerializer
+
+
+class UserListApiView(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserDetailSerializer
+    permission_classes = [IsAdmin]
+
+
+class UserCreateAPIView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class UserRetrieveApiView(RetrieveAPIView):
+    queryset = User.objects.all()
+    permission_classes = [IsAdmin | IsUser]
+
+    def get_serializer_class(self):
+        if self.get_object() == self.request.user:
+            return UserDetailSerializer
+
+
+class UserUpdateApiView(UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [~IsAdmin, IsUser]
+
+
+class UserDestroyApiView(DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [~IsAdmin, IsUser]
