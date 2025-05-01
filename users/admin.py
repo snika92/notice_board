4fa0db8948b58a5b77ea94bc809b1a9234c6856a
Veronika Surkova <surkova_nika92@mail.ru>
2025-05-01 20:50:07 +0300

    Add User to admin

--- users/admin.py
+++ users/admin.py
@@ -5,4 +5,12 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "first_name", "last_name", "email", "phone", "role", "is_active")
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "email",
+        "phone",
+        "role",
+        "is_active",
+    )
