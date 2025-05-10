30510d5c9048896ef4d49510af5f984b35acb1c8
Veronika Surkova <surkova_nika92@mail.ru>
2025-05-10 16:55:15 +0300

    Add settings

--- config/settings.py
+++ config/settings.py
@@ -140,10 +140,10 @@ if CACHE_ENABLED:
         }
     }
 
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:8000'
-]
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8000"]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://read-and-write.example.com",
 ]
+
+SWAGGER_USE_COMPAT_RENDERERS = False
